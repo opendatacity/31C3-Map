1529eb0afc234df6a8fe5ef429a6c892788e65d3
Michael Kreil <github@michael-kreil.de>
2014-12-26 17:26:26 +0100

    improve tile drawing

--- node/config/config.js
+++ node/config/config.js
@@ -1,29 +1,32 @@
 var path = require('path');
 var mainFolder = path.join(__dirname, '../../');
 
+var isoFloorMargin = -100;
+var topFloorMargin = -100;
+
 var config = {
 	imageFolder: mainFolder+'images/',
 	tileFolder: mainFolder+'web/tiles/',
 	usermapFolder: mainFolder+'../31c3-usermap/renderings/',
-	overview:{
+	overview: {
 		iso: {
-			width:6790, height:13670,
+			width:6790, height:13670 + 4*isoFloorMargin,
 			levels: [
-				{ x:250, y:8800 },
-				{ x:250, y:6600 },
-				{ x:250, y:4400 },
-				{ x:250, y:2200 },
-				{ x:250, y:   0 }
+				{ x:250, y:8800 + 4*isoFloorMargin },
+				{ x:250, y:6600 + 3*isoFloorMargin },
+				{ x:250, y:4400 + 2*isoFloorMargin },
+				{ x:250, y:2200 + 1*isoFloorMargin },
+				{ x:250, y:   0 + 0*isoFloorMargin }
 			]
 		},
 		top: {
-			width:5940, height:13092,
+			width:5940, height:13092 + 4*topFloorMargin,
 			levels: [
-				{ x:400, y:8715 },
-				{ x:400, y:6278 },
-				{ x:400, y:4212 },
-				{ x:400, y:2008 },
-				{ x:400, y:-208 }
+				{ x:400, y:8715 + 4*topFloorMargin },
+				{ x:400, y:6278 + 3*topFloorMargin },
+				{ x:400, y:4212 + 2*topFloorMargin },
+				{ x:400, y:2008 + 1*topFloorMargin },
+				{ x:400, y: 208 + 0*topFloorMargin }
 			]
 		}
 	}
