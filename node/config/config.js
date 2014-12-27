4543623aa3f1f5ec3c7548603ad6634460051922
Michael Kreil <github@michael-kreil.de>
2014-12-27 03:47:33 +0100

    update level positions

--- node/config/config.js
+++ node/config/config.js
@@ -1,32 +1,36 @@
 var path = require('path');
 var mainFolder = path.join(__dirname, '../../');
 
-var isoFloorMargin = -100;
-var topFloorMargin = -100;
-
 var config = {
 	imageFolder: mainFolder+'images/',
 	tileFolder: mainFolder+'web/tiles/',
 	usermapFolder: mainFolder+'../31c3-usermap/renderings/',
+	webConfigFile: mainFolder+'web/config.js',
 	overview: {
-		iso: {
-			width:6790, height:13670 + 4*isoFloorMargin,
+		top: {
+			title: 'Top Down',
+			name: 'top',
+			width: 5140,
+			height: 12690 - 1100,
 			levels: [
-				{ x:250, y:8800 + 4*isoFloorMargin },
-				{ x:250, y:6600 + 3*isoFloorMargin },
-				{ x:250, y:4400 + 2*isoFloorMargin },
-				{ x:250, y:2200 + 1*isoFloorMargin },
-				{ x:250, y:   0 + 0*isoFloorMargin }
+				{ x:0, y:8510 - 1100 },
+				{ x:0, y:6090 -  800 },
+				{ x:0, y:4010 -  600 },
+				{ x:0, y:1800 -  600 },
+				{ x:0, y:   0 -  400 }
 			]
 		},
-		top: {
-			width:5940, height:13092 + 4*topFloorMargin,
+		iso: {
+			title: 'Pseudo 3D',
+			name: 'iso',
+			width: 6790,
+			height: 13670 - 400,
 			levels: [
-				{ x:400, y:8715 + 4*topFloorMargin },
-				{ x:400, y:6278 + 3*topFloorMargin },
-				{ x:400, y:4212 + 2*topFloorMargin },
-				{ x:400, y:2008 + 1*topFloorMargin },
-				{ x:400, y: 208 + 0*topFloorMargin }
+				{ x:250, y:8800 - 400 },
+				{ x:250, y:6600 - 300 },
+				{ x:250, y:4400 - 200 },
+				{ x:250, y:2200 - 100 },
+				{ x:250, y:   0 - 000 }
 			]
 		}
 	}
