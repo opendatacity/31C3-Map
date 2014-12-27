4fd67746ef153774949afd773feb9b38ab4c8ef4
Michael Kreil <github@michael-kreil.de>
2014-12-27 04:44:14 +0100

    adding projection magic

--- web/assets/js/main.js
+++ web/assets/js/main.js
@@ -26,27 +26,137 @@ function Map() {
 	var map = L.map('map', {
 		minZoom: 0,
 		maxZoom: 8,
-		zoom: 0,
+		zoom: 2,
 		center: [-0.5*f,0.5*f],
 		maxBounds: [[-f,0],[0,f]],
 		crs: crs
 	});
-	
-	L.tileLayer('tiles/top/{z}/{x}/{y}.png', {
-		minZoom: 0,
-		maxZoom: 8,
-		maxNativeZoom: 6,
-		tileSize: retina ? 128 : 256,
-		zoomOffset: retina ? 1 : 0,
-		attribution: '<a href="https://events.ccc.de/congress/2013/">31C3</a> | Improve the code on <a href="https://github.com/OpenDataCity/31c3-map">GitHub</a>!',
-		noWrap: true
-	}).addTo(map);
+
+	config.map.tileLayers.forEach(function (layer) {
+		layer.leaflet = L.tileLayer(layer.tileUrl, {
+			minZoom: 0,
+			maxZoom: 8,
+			maxNativeZoom: 6,
+			tileSize: retina ? 128 : 256,
+			zoomOffset: retina ? 1 : 0,
+			attribution: '<a href="https://events.ccc.de/congress/2013/">31C3</a> | Improve the code on <a href="https://github.com/OpenDataCity/31c3-map">GitHub</a>!',
+			noWrap: true
+		})
+
+		P.addProjection(
+			layer.name,
+			function (p) {
+				var minD = 1e60;
+				var level, levelIndex;
+				layer.levels.forEach(function (_level, index) {
+					var d = sqr(p[0] - _level.c[0]) + sqr(p[1] - _level.c[1]);
+					if (d < minD) {
+						minD = d;
+						level = _level;
+						levelIndex = index;
+					}
+				})
+				var det = level.dx[0]*level.dy[1] - level.dy[0]*level.dx[1];
+				var x = (p[0] - level.p0[0])/det;
+				var y = (p[1] - level.p0[1])/det;
+				var result = [
+					levelIndex,
+					 x*level.dy[1] - y*level.dy[0],
+					-x*level.dx[1] + y*level.dx[0]
+				]
+				return result;
+			},
+			function (p) {
+				var level = layer.levels[p[0]];
+				return [
+					p[1]*level.dx[0] + p[2]*level.dy[0] + level.p0[0],
+					p[1]*level.dx[1] + p[2]*level.dy[1] + level.p0[1]
+				]
+			}
+		)
+	})
+
+	P.addProjection(
+		'map',
+		function (p) { return [-p[1],  p[0]] },
+		function (p) { return [ p[1], -p[0]] }
+	)
+
+	setTileLayer(0);
+
+	function setTileLayer(index) {
+		var layer = config.map.tileLayers[index]
+		layer.leaflet.addTo(map);
+
+		var bounds = [
+			P.project(layer.levels[0].c, 'global', 'map'),
+			P.project(layer.levels[4].c, 'global', 'map')
+		]
+		map.fitBounds(bounds);
+
+		/*
+		// Zeigt für die einzelnen Etagen an:
+		// - Bounding Box
+		// - Center Point
+		// - Separating Line
+		for (var i = 0; i <= 4; i++) {
+			var bounds = [
+				P.project([i,0,0], layer.name, 'map'),
+				P.project([i,5940,4320], layer.name, 'map')
+			]
+			L.rectangle(bounds, {weight:1, color:'#0f0', fill:false }).addTo(map);
+			L.circleMarker(
+				P.project(layer.levels[i].c, 'global', 'map'),
+				{weight:1, color:'#f00'}).addTo(map);
+		}
+
+		for (var i = 0; i < 4; i++) {
+			var c0 = layer.levels[i  ].c;
+			var c1 = layer.levels[i+1].c;
+			var c = [(c0[0]+c1[0])/2, (c0[1]+c1[1])/2];
+			var d = [(c0[0]-c1[0])*2, (c0[1]-c1[1])*2];
+			var line = [
+				P.project([c[0]-d[1],c[1]+d[0]], 'global', 'map'),
+				P.project([c[0]+d[1],c[1]-d[0]], 'global', 'map')
+			]
+			L.polyline(line, {weight:1, color:'#f00'}).addTo(map);
+		}
+		*/
+	}
 
 	map.on('mousemove', function (e) {
-		$('#searchtext').val(e.latlng)
+		$('#searchtext').val(P.project([e.latlng.lat, e.latlng.lng], 'map', 'top'))
 	})
+
+	function sqr(x) {
+		return x*x;
+	}
 }
 
+var P = function () {
+	// Globale Projektion ist [0..16384,0..16384], euklidisch, wie ein großes Bild, mit [0,0] links oben.
+	var projections = {};
+	function addProjection (name, funcTo, funcFrom) {
+		projections[name] = {
+			funcTo:  funcTo,
+			funcFrom:funcFrom
+		}
+	}
+	function project (point, nameFrom, nameTo) {
+		return projections[nameTo].funcTo(projections[nameFrom].funcFrom(point));
+	}
+
+	addProjection(
+		'global',
+		function (p) { return p },
+		function (p) { return p }
+	)
+
+	return {
+		addProjection: addProjection,
+		project: project
+	}
+}()
 
 
 
